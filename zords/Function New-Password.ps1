ff6f694238c051b6ea8e7983b9aa05c9dab16193
Baron Washington <shalom.washington@venture1consulting.com>
2022-05-13 15:11:47 +0100

    folder for the different mini-sections

---
+++ zords/Function New-Password.ps1
@@ -0,0 +1,17 @@
+Function New-Password {
+    $uppercase = "ABCDEFGHKLMNOPRSTUVWXYZ".tochararray() 
+    $lowercase = "abcdefghiklmnoprstuvwxyz".tochararray() 
+    $number = "0123456789".tochararray() 
+    $special = "$%&/()=?}{@#*+!".tochararray()
+    $complexity = @($uppercase, $lowercase, $number, $special)
+
+    $password =($complexity | Get-Random -count  15) -join ''
+
+    $passwordarray=$password.tochararray() 
+    $scrambledpassword = ($passwordarray | Get-Random -Count 15) -join ''
+
+    Write-Output $scrambledpassword
+}
+
+New-Password
+$scrambledpassword
