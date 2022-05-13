cdb3b6b63d48e97c2e667a1100425070de5c1ab6
Baron Washington <shalom.washington@venture1consulting.com>
2022-05-13 15:11:12 +0100

    just updating what I've done so far

--- Set-LocalAdminPassword.ps1
+++ Set-LocalAdminPassword.ps1
@@ -1,34 +1,17 @@
-<#
-The Password Settings are as follows:
- - Length (Characters): 15
- - Complexity: Multicase + Numbers + Specials
- #>
+ Function New-Password {
+    $uppercase = "ABCDEFGHKLMNOPRSTUVWXYZ".tochararray() 
+    $lowercase = "abcdefghiklmnoprstuvwxyz".tochararray() 
+    $number = "0123456789".tochararray() 
+    $special = "$%&/()=?}{@#*+!".tochararray()
+    $complexity = @($uppercase, $lowercase, $number, $special)
 
- 
-$uppercase = "ABCDEFGHKLMNOPRSTUVWXYZ".tochararray() 
-$lowercase = "abcdefghiklmnoprstuvwxyz".tochararray() 
-$number = "0123456789".tochararray() 
-$special = "$%&/()=?}{@#*+!".tochararray() 
+    $password =($complexity | Get-Random -count  15) -join ''
 
-$password =($uppercase | Get-Random -count 1) -join ''
-$password +=($lowercase | Get-Random -count 5) -join ''
-$password +=($number | Get-Random -count 1) -join ''
-$password +=($special | Get-Random -count 1) -join ''
+    $passwordarray=$password.tochararray() 
+    $scrambledpassword = ($passwordarray | Get-Random -Count 15) -join ''
 
-$passwordarray=$password.tochararray() 
-$scrambledpassword=($passwordarray | Get-Random -Count 8) -join ''
-$scrambledpassword
-
-
-
-
-<#
-function New-Password {
-    
-$message = "Hello" #Define Variable
-Write-Output $message #Returns Password
+    Write-Output $scrambledpassword
 }
 
-New-Password # Runs function
-$message
-#net user #>
+New-Password
+$scrambledpassword
